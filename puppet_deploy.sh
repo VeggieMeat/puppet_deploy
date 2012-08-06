a5f663c40f943dcbcd88f406801d473c920f4b7b
Brian Altenhofel <brian.altenhofel@vmdoh.com>
2012-08-05 20:47:20 -0500

    Shell script added

---
+++ puppet_deploy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#
+# Fabfile wrapper script
+#
+
+# Fabfile location
+FABFILE=/usr/local/bin/puppet_deploy/fabfile.py
+
+LIST=$1
+MASTER=$2
+
+exec < $LIST
+while read LINE
+  do
+    HOSTS=$HOSTS','$LINE
+  done
+
+HOSTLIST=$(echo $HOSTS | awk '{print substr($0,2)}')
+fab -f $FABFILE -H $HOSTLIST backup:master=$MASTER || exit 1

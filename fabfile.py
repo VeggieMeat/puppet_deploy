3e6cbd57c02933c15968b1708f1023295d59f6f7
Brian Altenhofel <brian.altenhofel@vmdoh.com>
2012-08-05 21:08:58 -0500

    Added user and shell and fixed typo

--- fabfile.py
+++ fabfile.py
@@ -1,5 +1,7 @@
 from fabric.api import *
+env.user = 'bender'
+env.shell = '/bin/bash -c'
 
 @parallel
 def puppet(master):
-  run("sudo puppet agent --server=%s --no-daemonize --onetime --verbose", %(master))
+  run("sudo puppet agent --server=%s --no-daemonize --onetime --verbose" %(master))
